611a689810f19ffba66721241a913e9f9c18fa35
jeremyBass <jeremybass@cableone.net>
2014-04-10 12:56:56 -0700

    use literal string compare

--- glassceiling.sh
+++ glassceiling.sh
@@ -4,7 +4,7 @@ cronfile=glassceiling.sh    #NOTE THIS SHOULD DETECT IT'S SELF
 path=$(pwd)                   #this is the path to the file 
 percent_allowed=80            #this should be max memory before action
 
-if[ "$path" == "/" ]
+if[[ "$path" == "/" ]]
 then
     path=""
 fi
