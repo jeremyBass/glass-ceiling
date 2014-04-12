3bd93bf6446709ae7d59b3da08afa48982047375
Jeremy Bass <jeremybass@cableone.net>
2014-04-12 10:14:05 -0700

    tabs are the American express of text

--- glassceiling.sh
+++ glassceiling.sh
@@ -10,7 +10,7 @@ path=$(pwd)                   #this is the path to the file
 
 
 if [ -z "$1" ]; then
-    percent_allowed=80
+	percent_allowed=80
 else
 	percent_allowed="$1"          #this should be max memory before action
 	echo "$(date) --memory limit set at $percent_allowed%" >> /cron.log
@@ -26,50 +26,50 @@ PERCENTAGE=0
 
 
 if [[ "$path" == "/" ]]; then
-    path=""
+	path=""
 fi
 
 # Sets up the crontab for this
 touch cron.log
 has_cron(){
-    #is the file in the cron?
-    #return 0 #returning this just to test should
-    $(crontab -l | egrep -v '^$|^#' | grep -q $cronfile;) && return 1 || return 0
+	#is the file in the cron?
+	#return 0 #returning this just to test should
+	$(crontab -l | egrep -v '^$|^#' | grep -q $cronfile;) && return 1 || return 0
 }
 if has_cron;
 then
-    crontab -l > mycron
-    # Echo new cron into cron file
-    echo "*/5 * * * * sh $path/$cronfile" >> mycron
-    # Install new cron file
-    crontab mycron
-    rm mycron
-    echo "$(date) --Installation sucess, memory limit is at $percent_allowed%" >> /cron.log
+	crontab -l > mycron
+	# Echo new cron into cron file
+	echo "*/5 * * * * sh $path/$cronfile" >> mycron
+	# Install new cron file
+	crontab mycron
+	rm mycron
+	echo "$(date) --Installation sucess, memory limit is at $percent_allowed%" >> /cron.log
 fi
 
 
 
 # sets up the function of this script
 test_memory(){
-    memusage=$(top -n 1 -b | grep "Mem")
-    MAXMEM=$(echo $memusage | cut -d" " -f2 | awk '{print substr($0,1,length($0)-1)}')
-    USEDMEM=$(echo $memusage | cut -d" " -f4 | awk '{print substr($0,1,length($0)-1)}')
+	memusage=$(top -n 1 -b | grep "Mem")
+	MAXMEM=$(echo $memusage | cut -d" " -f2 | awk '{print substr($0,1,length($0)-1)}')
+	USEDMEM=$(echo $memusage | cut -d" " -f4 | awk '{print substr($0,1,length($0)-1)}')
 
-    USEDMEM1=$(expr $USEDMEM \* 100)
-    PERCENTAGE=$(expr $USEDMEM1 / $MAXMEM)
-    
-    [[ $PERCENTAGE>$percent_allowed ]] && return 0 || return 1
+	USEDMEM1=$(expr $USEDMEM \* 100)
+	PERCENTAGE=$(expr $USEDMEM1 / $MAXMEM)
+	
+	[[ $PERCENTAGE>$percent_allowed ]] && return 0 || return 1
 }
 
 
 if test_memory;
 then
-    echo "$(date) --mem is critical, $PERCENTAGE% ($USEDMEM), restarting -- $path/$cronfile " >> /cron.log
-    echo $(/etc/init.d/php-fpm restart) 1>&2 >> /cron.log
-    echo $(/etc/init.d/nginx restart) 1>&2 >> /cron.log
-    #echo "It seems that you're out of memory and luck" | mutt -a "/cron.log" -s "OUT of Memory" -- recipient@domain.com
+	echo "$(date) --mem is critical, $PERCENTAGE% ($USEDMEM), restarting -- $path/$cronfile " >> /cron.log
+	echo $(/etc/init.d/php-fpm restart) 1>&2 >> /cron.log
+	echo $(/etc/init.d/nginx restart) 1>&2 >> /cron.log
+	#echo "It seems that you're out of memory and luck" | mutt -a "/cron.log" -s "OUT of Memory" -- recipient@domain.com
 
 else
-    echo "$(date) --mem is ok  -- $path/$cronfile "  >> /cron.log
+	echo "$(date) --mem is ok  -- $path/$cronfile "  >> /cron.log
 fi
 exit 0
